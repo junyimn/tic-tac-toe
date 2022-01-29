fbc58e1f76d2a7a8a497de85aa8e81a910f41213
junyimn <junyimn@outlook.com>
2022-01-29 13:11:17 +0000

    testing

---
+++ player.py
@@ -0,0 +1,25 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        # letter is x or o
+
+    # we want all players to get thier next move
+
+    def get_move(self, game):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+class HumanPlater(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    
